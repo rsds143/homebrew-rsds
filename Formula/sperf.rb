de9b558722369bc46e715d4bce7bd71e157232a0
Ryan SVIHLA <tech@foundev.pro>
2021-04-26 10:33:54 +0200

    updating sperf

--- Formula/sperf.rb
+++ Formula/sperf.rb
@@ -1,23 +1,10 @@
-#  Copyright 2021 Ryan Svihla
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
 class Sperf < Formula
   include Language::Python::Virtualenv
 
   desc "Datastax Enterprise and Apache Cassandra diagnostic tooling from DataStax"
   homepage "https://github.com/DataStax-Toolkit/sperf"
-  url "https://github.com/DataStax-Toolkit/sperf/archive/v0.6.7.tar.gz"
-  sha256 "332c75f42cc7a858660c4e9d3b590e53d6446833a4c2e3bab6f750400de47d6e"
+  url "https://github.com/DataStax-Toolkit/sperf/archive/v0.6.8.tar.gz"
+  sha256 "32cad3c94728289dae314d47dd484fa4f418bcaf8fcebe591bf9cfaa2fe56ed5"
   license "Apache-2.0"
   depends_on "python@3.9"
 
